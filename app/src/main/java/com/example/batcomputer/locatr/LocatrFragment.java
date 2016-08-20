e12062b145120f97187d10c9e1d777cffc53e455
Akash-Mishra <mishraakash18@gmail.com>
2016-08-20 23:26:01 +0530

    LocatrFragment.java class added

---
+++ app/src/main/java/com/example/batcomputer/locatr/LocatrFragment.java
@@ -0,0 +1,16 @@
+package com.example.batcomputer.locatr;
+
+import android.app.Fragment;
+import android.widget.ImageView;
+
+/**
+ * Created by Bat Computer on 20-08-2016.
+ */
+public class LocatrFragment extends Fragment {
+    private ImageView mImageView;
+    public static LocatrFragment newInstance() {
+        return new LocatrFragment();
+    }
+
+//    @Override
+}
