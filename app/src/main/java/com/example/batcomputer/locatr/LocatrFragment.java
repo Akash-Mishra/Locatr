d7a918bed1a8f084a7a323c4c349324558146060
Akash-Mishra <mishraakash18@gmail.com>
2016-08-21 00:23:54 +0530

    Locatr Fragment

--- app/src/main/java/com/example/batcomputer/locatr/LocatrFragment.java
+++ app/src/main/java/com/example/batcomputer/locatr/LocatrFragment.java
@@ -3,6 +3,8 @@ package com.example.batcomputer.locatr;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -17,10 +19,21 @@ public class LocatrFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View v = inflater.inflate(R.layout.fragment_locatr, container, false);
         mImageView = (ImageView) v.findViewById(R.id.image);
         return v;
+    }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu,MenuInflater inflater){
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_locatr, menu);
     }
 }
